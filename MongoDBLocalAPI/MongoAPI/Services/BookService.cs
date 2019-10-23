c8a1443aa23f235039ff68233e96a911c47f1205
spels47 <34216925+spels47@users.noreply.github.com>
2019-10-23 12:26:43 +0200

    x

---
+++ MongoDBLocalAPI/MongoAPI/Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using MongoAPI.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoAPI.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> documents;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            documents = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() =>
+            documents.Find(book => true).ToList();
+
+        public Book Get(string id) =>
+            documents.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            documents.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            documents.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            documents.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            documents.DeleteOne(book => book.Id == id);
+    }
+}
