021839fe67709af9180addd913a4d299611315e7
spels47 <34216925+spels47@users.noreply.github.com>
2019-10-23 12:26:03 +0200

    Create ServeDocument.cs

---
+++ MongoDBLocalAPI/MongoAPI/Models/ServeDocument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoAPI.Models
+{
+    public class ServeDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string DocName { get; set; }
+
+        public Byte[] file { get; set; }
+    }
+}
