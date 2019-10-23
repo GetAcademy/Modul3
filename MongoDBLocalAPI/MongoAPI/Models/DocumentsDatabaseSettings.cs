b9b60eb9f5c354d31a862728b0584848f037a8df
spels47 <34216925+spels47@users.noreply.github.com>
2019-10-23 12:25:53 +0200

    Create DocumentsDatabaseSettings.cs

---
+++ MongoDBLocalAPI/MongoAPI/Models/DocumentsDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoAPI.Models
+{
+    public class DocumentsDatabaseSettings : IDocumentsDatabaseSettings
+    {
+        public string DocumentsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface IDocumentsDatabaseSettings
+    {
+        string DocumentsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
