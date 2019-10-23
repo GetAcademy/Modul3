3a39afb698e7059c18182ecc0bc927a0b4c4f4d7
spels47 <34216925+spels47@users.noreply.github.com>
2019-10-23 12:25:43 +0200

    Create Documents.cs

---
+++ MongoDBLocalAPI/MongoAPI/Models/Documents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace MongoAPI.Models
+{
+    public class Documents
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string DocumentName { get; set; }
+
+        public string Document { get; set; }
+    }
+}
